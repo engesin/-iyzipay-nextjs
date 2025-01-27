e4370142fb662e6e32b0c33bf7c8dae3c40456fe
engin.esin <engesin@gmail.com>
2025-01-27 10:31:30 +0300

    logging

--- src/app/actions/checkout.ts
+++ src/app/actions/checkout.ts
@@ -70,6 +70,7 @@ export async function initiateCheckoutForm(): Promise<CheckoutFormInitResponse> 
         if (err) {
           reject(err);
         } else {
+          console.log("CheckoutFormInitResponse", result);
           resolve(result);
         }
       });
@@ -93,6 +94,7 @@ export async function retrieveCheckoutForm(
         if (err) {
           reject(err);
         } else {
+          console.log("CheckoutFormResponse", result);
           resolve(result);
         }
       });
